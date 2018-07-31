f6a4dd10f43436ec2638e8ab1cef6cc65272582c
NerDor <37439240+NerDor@users.noreply.github.com>
2018-07-31 17:47:12 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@
  */
 return [
     'username' => 'root',           //设置账号
-    'password' => 'zB75Xp2EaE',      //设置密码
+    'password' => '',      //设置密码
     'table_prefix'=>'ims_',
-    'dsn' => 'mysql:host=10.66.238.196;dbname=fxh;port=3306;charset=utf8',//数据库信息
+    'dsn' => 'mysql:host=;dbname=fxh;port=3306;charset=utf8',//数据库信息
 ];
